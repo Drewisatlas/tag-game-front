22e9a0496e8d25f5d4d7d0e46122931b4aee0aaf
Drewisatlas <drewisatlas@gmail.com>
2019-01-11 16:09:57 -0500

    about to build random movements for cpu player

--- src/gameLogic/startGame.js
+++ src/gameLogic/startGame.js
@@ -1,5 +1,26 @@
-const startGame = () => {
+import store from '../config/store'
 
+let state = store.getState();
+console.log(state)
+
+
+const startPhase = () => {
+
+}
+
+const randomCpuMovement = () => {
+debugger
+ console.log('Pete Wentz has moved')
+ let coordinates= state.cpuPlayer.gridArea.split('/')
+ let xCoord = parseInt(coordinates[1])
+ let yCoord = parseInt(coordinates[0])
+ console.log(`${xCoord}, ${yCoord}`)
+ let newCoordinates = this.props.cpuPlayer.gridArea
+}
+
+
+const startGame = () => {
+  setInterval(randomCpuMovement, 3000);
 }
 
 export default startGame
