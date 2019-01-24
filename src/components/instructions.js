592ae78206d12b08e2265cbdc8d7b4a3024a45f8
Drewisatlas <drewisatlas@gmail.com>
2019-01-24 12:44:04 -0500

    stable version with instructions

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {startScreenAC} from '../actions/appActions.js'
+import andrew from '../sprites/player1.png'
+import pete from '../sprites/player2.png'
+import jeremy from '../sprites/player3.png'
+import charlie from '../sprites/player4.png'
+import tagZone from '../instructionPics/tagZone.png'
+import tagged from '../instructionPics/youGotTagged.png'
 
 class Instructions extends React.Component {
 
@@ -11,22 +17,26 @@ class Instructions extends React.Component {
   render () {
     return (
       <div className='instructionsContainer'>
-        <div />
+        <div>  </div>
           <div className='instructionsArea'>
-            <h1 style={{textAlign:'center'}}> How To Play Tag! </h1>
-            <p> There are 4 players in the game </p>
-            <p> You are "Andrew" the dashingly handsome 8-bit hero with brown hair!</p>
-            <p> "Pete" plays in a succesful Pop-Punk band and has black hair.</p>
-            <p> "Jeremy" is Andrew's best friend and has blonde hair.</p>
-            <p> Charlie is always the first player to be "It" (because he is the youngest) he has red hair.</p>
-            <p> Starting with You, each player takes turns moving around the board, three spaces per turn.</p>
-            <p> If the player that is "It" ends their turn and another player is within a one square radius, that player will be tagged.</p>
-            <p> When a player is tagged they will lose one heart, and it is their turn to be it and try to tag another player. </p>
-            <p> There ARE tag backs, so be careful who you tag. </p>
-            <p> The last Player Standing is the Winner! </p>
+            <div style={{}}> <h1> How To Play Tag! </h1>  </div>
+            <div style={{}}> <h3> There are 4 players in the game </h3>  </div>
+            <div style={{backgroundImage:`url(${andrew})`, backgroundPosition: '0 0', width: '50px', height: '73px',}} />
+            <div> <p> You are "Andrew" the dashingly handsome 8-bit hero with brown hair!</p> </div>
+              <div style={{backgroundImage:`url(${pete})`, backgroundPosition: '0 0', width: '50px', height: '73px',}} />
+            <div> <p> "Pete" plays in a succesful Pop-Punk band and has black hair.</p> </div>
+              <div style={{backgroundImage:`url(${jeremy})`, backgroundPosition: '0 0', width: '50px', height: '73px',}} />
+            <div> <p> "Jeremy" is Andrew's best friend and has blonde hair.</p> </div>
+                <div style={{backgroundImage:`url(${andrew})`, backgroundPosition: '0 0', width: '50px', height: '73px',}} />
+            <div> <p> "Charlie" is always the first player to be "It" (because he is the youngest) he has red hair.</p> </div>
+            <div> <p> Starting with You, each player takes turns moving around the board, three spaces per turn.</p> </div>
+            <div> <p> If the player that is "It" ends their turn and another player is within a one square radius, that player will be tagged.</p> </div>
+            <div> <p> When a player is tagged they will lose one heart, and it is their turn to be it and try to tag another player. </p> </div>
+            <div> <p> There ARE tag backs, so be careful who you tag. </p> </div>
+            <div> <p> The last Player Standing is the Winner! </p></div>
             <div style={{textAlign:'center'}} onClick={this.goBack}> <h2> Back </h2> </div>
           </div>
-        <div />
+        <div> </div>
       </div>
     )
   }
